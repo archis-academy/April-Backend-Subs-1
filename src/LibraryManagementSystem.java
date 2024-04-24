ce53bce945f38ea231ee2cb7faff6c4ed6ce75e3
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2024-04-24 14:24:58 +0300

    Initial commit

---
+++ src/LibraryManagementSystem.java
@@ -0,0 +1,14 @@
+public class LibraryManagementSystem {
+
+    private static final int INDEX = 100;
+    private static final String[][] books = new String[INDEX][4];
+    private static final String[][] users = new String[INDEX][4];
+    private static final String[][] transactions = new String[INDEX][4];
+    private static int bookQuantity = 0;
+    private static int userQuantity = 0;
+    private static int transactionQuantity = 0;
+
+    public static void main(String[] args) {
+
+    }
+}
