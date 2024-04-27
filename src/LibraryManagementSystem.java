ad409a8f4d28b07e4c8cb6d5cdf3eaf93e5275a9
emineaycakul <aycakul@outlook.com>
2024-04-27 16:11:52 +0300

    Emine/[AP-3] - AddBook() method

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -11,4 +11,39 @@ public class LibraryManagementSystem {
     public static void main(String[] args) {
 
     }
+    
+    public static void addBook (String tittle, String author, String ISBN, String quantity) {
+    	boolean isExist = false;
+    	int indexOfExistBook = -1;
+    	for (int i = 0; i < books.length; i++) {
+			if (tittle.equals(books[i][0]) && author.equals(books[i][1]) && ISBN.equals(books[i][2])) {
+				isExist = true;
+				indexOfExistBook = i;
+				break;
+			}
+		}
+    	
+    	if (isExist) {
+			books[indexOfExistBook][3] = String.valueOf(Integer.valueOf(books[indexOfExistBook][3]) + Integer.valueOf(quantity));
+			bookQuantity += Integer.valueOf(quantity);
+		}
+    	
+    	else {
+        	int emptyIndex = -1;
+        	for (int i = 0; i < books.length; i++) {
+    			if (books[i] == null) {
+    				emptyIndex = i;
+    				break;
+    			}
+    		}
+        	books[emptyIndex][0] = tittle;
+        	books[emptyIndex][1] = author;
+        	books[emptyIndex][2] = ISBN;
+        	books[emptyIndex][3] = quantity;
+        	bookQuantity += Integer.valueOf(quantity);
+		}
+    	
+    	transactionQuantity++;
+    	System.out.println("Kitap ekleme işlemi başarıyla tamamlandı.");
+    }
 }
