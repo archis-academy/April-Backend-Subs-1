a55500f8b6e3c4d2502735ed0a283ac55d95b8ce
emineaycakul <aycakul@outlook.com>
2024-04-30 02:21:40 +0300

    Update [AP-3] - add book method

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -3,7 +3,7 @@ public class LibraryManagementSystem {
     private static final int INDEX = 100;
     private static final String[][] books = new String[INDEX][4];
     private static final String[][] users = new String[INDEX][4];
-    private static final String[][] transactions = new String[INDEX][4];
+    private static final String[][] transactions = new String[INDEX][3];
     private static int bookQuantity = 0;
     private static int userQuantity = 0;
     private static int transactionQuantity = 0;
@@ -12,22 +12,30 @@ public class LibraryManagementSystem {
 
     }
     
-    public static void addBook (String tittle, String author, String ISBN, String quantity) {
-    	boolean isExist = false;
+    /*
+     * This method checks whether the book with the given ISBN exists in the library.
+     * If the book exists, it returns the index of the book; otherwise, it returns -1.
+     */
+    public static int getBookIndexByISBN (String ISBN) {
     	int indexOfExistBook = -1;
     	for (int i = 0; i < books.length; i++) {
-			if (tittle.equals(books[i][0]) && author.equals(books[i][1]) && ISBN.equals(books[i][2])) {
-				isExist = true;
+			if (ISBN.equals(books[i][2])) {
 				indexOfExistBook = i;
 				break;
 			}
 		}
-    	
-    	if (isExist) {
-			books[indexOfExistBook][3] = String.valueOf(Integer.valueOf(books[indexOfExistBook][3]) + Integer.valueOf(quantity));
-			bookQuantity += Integer.valueOf(quantity);
+    	return indexOfExistBook;
+    }
+    
+    /*
+     * This method adds the given book to the library.
+     * If the book exists, it prints a message indicating this.
+     * Otherwise, it prints a message that the book was successfully added.
+     */
+    public static void addBook (String tittle, String author, String ISBN, String pageNumber) {
+    	if (getBookIndexByISBN(ISBN) == -1) {
+			System.out.println("This book is already available in the library.");
 		}
-    	
     	else {
         	int emptyIndex = -1;
         	for (int i = 0; i < books.length; i++) {
@@ -36,14 +44,14 @@ public class LibraryManagementSystem {
     				break;
     			}
     		}
+        	
         	books[emptyIndex][0] = tittle;
         	books[emptyIndex][1] = author;
         	books[emptyIndex][2] = ISBN;
-        	books[emptyIndex][3] = quantity;
-        	bookQuantity += Integer.valueOf(quantity);
+        	books[emptyIndex][3] = pageNumber;
+        	bookQuantity++;
+        	
+        	System.out.println("Adding book has been completed successfully.");
 		}
-    	
-    	transactionQuantity++;
-    	System.out.println("Kitap ekleme işlemi başarıyla tamamlandı.");
     }
 }
