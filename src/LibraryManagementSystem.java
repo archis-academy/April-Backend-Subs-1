43aa344f3c9882f3cfb4503775b61d0e13124f96
emineaycakul <aycakul@outlook.com>
2024-04-30 22:44:31 +0300

    [AP-2] - search books method

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -11,4 +11,37 @@ public class LibraryManagementSystem {
     public static void main(String[] args) {
 
     }
+    
+    /*
+     * This method takes a string value as a parameter (title, author or ISBN)
+     * and lists the name, author, ISBN and page numbers of the book or books that match this string value.
+     */
+    public static void searchBooks (String string) {
+    	for (int i = 0; i < books.length; i++) {
+			if (books[i][0] == string) {
+				System.out.println("Tittle: " + books[i][0]);
+				System.out.println("Author: " + books[i][1]);
+				System.out.println("ISBN: " + books[i][2]);
+				System.out.println("Page Number: " + books[i][3]);
+			}
+			else if (books[i][1] == string) {
+				System.out.println("Tittle: " + books[i][0]);
+				System.out.println("Author: " + books[i][1]);
+				System.out.println("ISBN: " + books[i][2]);
+				System.out.println("Page Number: " + books[i][3]);
+			}
+			else if (books[i][2] == string) {
+				System.out.println("Tittle: " + books[i][0]);
+				System.out.println("Author: " + books[i][1]);
+				System.out.println("ISBN: " + books[i][2]);
+				System.out.println("Page Number: " + books[i][3]);
+			}
+			else if (books[i][3] == string) {
+				System.out.println("Tittle: " + books[i][0]);
+				System.out.println("Author: " + books[i][1]);
+				System.out.println("ISBN: " + books[i][2]);
+				System.out.println("Page Number: " + books[i][3]);
+			}
+		}
+    }
 }
