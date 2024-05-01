4e375467ffcf78a11db0d251bc6ec5c01bb84d68
Jamshid(Timur)  Sadik <77556948+JamshidS@users.noreply.github.com>
2024-05-01 14:06:53 +0300

    Update LibraryManagementSystem.java

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -1,9 +1,9 @@
 public class LibraryManagementSystem {
 
     private static final int INDEX = 100;
-    private static final String[][] books = new String[INDEX][4];
-    private static final String[][] users = new String[INDEX][4];
-    private static final String[][] transactions = new String[INDEX][3];
+    private static final String[][] books = new String[INDEX][4]; // [BookName,AutherName,ISBN,pageNumber]
+    private static final String[][] users = new String[INDEX][4]; // [FullName,ID,Email,Password]
+    private static final String[][] transactions = new String[INDEX][3]; // [userId,bookId,date]
     private static int bookQuantity = 0;
     private static int userQuantity = 0;
     private static int transactionQuantity = 0;
