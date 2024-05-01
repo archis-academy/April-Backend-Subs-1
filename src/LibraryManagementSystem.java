bb7390602e7383b72feeb19607ea9b47d345895c
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2024-05-01 14:31:34 +0300

    update checkBook method

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -1,7 +1,7 @@
 public class LibraryManagementSystem {
 
     private static final int INDEX = 100;
-    private static final String[][] books = new String[INDEX][4]; // [BookName,AutherName,ISBN,pageNumber]
+    private static final String[][] books = new String[INDEX][4]; // [BookName,AuthorName,ISBN,pageNumber]
     private static final String[][] users = new String[INDEX][4]; // [FullName,ID,Email,Password]
     private static final String[][] transactions = new String[INDEX][3]; // [userId,bookId,date]
     private static int bookQuantity = 0;
@@ -13,13 +13,15 @@ public class LibraryManagementSystem {
     }
 
     // Book Check
-    public  static  boolean  isCheckBooks(String ISBN){
-        for (int i=0; i<books.length;i++){
-            if(books[i][2] == ISBN){
-                return  true;
+    public  static  boolean  checkBook(String ISBN){
+        boolean response = false;
+        for (int i=0; i<bookQuantity;i++){
+            if(books[i][2].equals(ISBN)){
+                response = true;
+                break;
             }
         }
-        return false;
+        return response;
     }
 }
 
