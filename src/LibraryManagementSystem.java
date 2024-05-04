ac250b007d23ebaa0be8aed8b1774c59c4be8646
Berkan <b.sevil326@gmail.com>
2024-05-04 16:06:29 +0300

    [AP-14]-count total books method

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -9,7 +9,7 @@ public class LibraryManagementSystem {
     private static int transactionQuantity = 0;
 
     public static void main(String[] args) {
-
+    countTotalBooks();
     }
 
     // Book Check
@@ -23,5 +23,10 @@ public class LibraryManagementSystem {
         }
         return response;
     }
-}
 
+   // CountTotalBook
+    public static void countTotalBooks(){
+        int totalBooks=bookQuantity;
+        System.out.println("Total number of the books: " + totalBooks);
+    }
+}
