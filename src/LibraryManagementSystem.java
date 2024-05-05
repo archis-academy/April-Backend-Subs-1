11426cd5a65957f1d092df1bf986c70e3579bebe
emineaycakul <aycakul@outlook.com>
2024-05-06 01:51:40 +0300

    [AP-22] - check patron eligibility method

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -9,7 +9,61 @@ public class LibraryManagementSystem {
     private static int transactionQuantity = 0;
 
     public static void main(String[] args) {
- 
+    	
+    }
+    
+    /*
+     * This method checks whether the specified user is eligible to borrow a book from the library.
+     * Firstly, it checks if the user has any transaction record in the transactions array.
+     * If not, it returns true, (the user is eligible to borrow a book).
+     * If there is a transaction record, it also checks if the book they borrowed is available in the library.
+     * If the book is found (the user has returned the book), it returns true.
+     * If the book is not found, (the user has not returned the book yet), it returns false.
+     */
+    public static boolean checkPatronEligibilityForCheckout(String userId) {
+    	boolean eligibility = true;
+    	int transactionIndex = getTransactionIndexByUserId(userId);
+    	
+    	if (transactionIndex == -1) {
+    		eligibility = true;
+		}
+    	else {
+			String ISBN = transactions[transactionIndex][1];
+			if (getBookIndexByISBN(ISBN) == -1) {
+				eligibility = false;
+			}
+		}
+    	return eligibility;
+    }
+    
+    /*
+     * This method takes the user ID as a parameter and returns the index of the same user in the 'transactions' array.
+     * If the user has made no transactions and is not in the array, it returns -1.
+     */
+    public static int getTransactionIndexByUserId(String userId) {
+    	int transactionIndex = -1;
+    	for (int i = 0; i < transactionQuantity; i++) {
+			if (transactions[i][0].equals(userId)) {
+				transactionIndex = i;
+				break;
+			}
+		}
+    	return transactionIndex;
+    }
+    
+    /*
+     * This method checks whether the book with the given ISBN exists in the library.
+     * If the book exists, it returns the index of the book; otherwise, it returns -1.
+     */
+    public static int getBookIndexByISBN(String ISBN) {
+    	int indexOfExistBook = -1;
+    	for (int i = 0; i < bookQuantity; i++) {
+			if (ISBN.equals(books[i][2])) {
+				indexOfExistBook = i;
+				break;
+			}
+		}
+    	return indexOfExistBook;
     }
 
     // Book Check
